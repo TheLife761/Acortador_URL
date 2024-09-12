c5e9c6742601d0e5d67134c06eb9f61d8f579f5d
Gabriel Andrade <gabrielandraderoso@gmail.com>
2024-09-12 18:32:57 -0300

    add tests

--- src/tests/test.js
+++ src/tests/test.js
@@ -9,4 +9,18 @@ describe('IsValidURL', function () {
       assert.equal(isValidUrl("google.com"), false);
       assert.equal(isValidUrl(".com"), false);
     });
+
+    it('should accept valid http urls', function () {
+      assert.equal(isValidUrl('http://MVSXX.COMPANY.COM'), true);
+      assert.equal(isValidUrl('http://www.grose.us'), true);
+      assert.equal(isValidUrl('http://www.southcn.com/'), true);
+      assert.equal(isValidUrl('http://shippingchina.com/'), true);
+      assert.equal(isValidUrl('http://icio.us/'), true);
+    });
+
+    it('should accept valid https urls', function () {
+      assert.equal(isValidUrl('https://www.google.com'), true);
+      assert.equal(isValidUrl('https://amazon.com'), true);
+      assert.equal(isValidUrl('https://facebook.com'), true);
+    });
 });

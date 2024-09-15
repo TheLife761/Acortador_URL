f35adde5f3006c22eb05323046bd95a717297216
Gabriel Andrade <gabrielandraderoso@gmail.com>
2024-09-15 20:50:45 -0300

    make test use process env host

--- src/tests/get-request-test.js
+++ src/tests/get-request-test.js
@@ -8,10 +8,12 @@ require('dotenv').config();
 const expect = chai.expect;
 chai.use(chaiHttp);
 
+const SERVER = process.env.HOST + ":" + process.env.PORT;
+
 describe('GET /', () => {
   it("Should return a valid response when requesting index", function (done) {
     const server = process.env.HOST + ":" + process.env.PORT;
-    chai.request(server)
+    chai.request(SERVER)
       .get('/')
       .end(function (err, res) {
         expect(err).to.be.null;
@@ -22,8 +24,9 @@ describe('GET /', () => {
 });
 
 describe('GET /:urlID', () => {
+  
   it("Should return a status code of 404 for a non-existent URL ID", function (done) {
-    chai.request("http://localhost:3000")
+    chai.request(SERVER)
       .get('/404')
       .end(function (err, res) {
         expect(err).to.be.null;
--- src/tests/post-request-test.js
+++ src/tests/post-request-test.js
@@ -6,12 +6,13 @@ var chaiHttp = require('chai-http');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
+const SERVER = process.env.HOST + ":" + process.env.PORT;
+
 describe('POST /api/shortener', function () {
-    var host = "http://localhost:3000";
-    var path = "/api/shortener";
+    const path = "/api/shortener";
 
     it('Should be able to send parameters to /api/shortener via POST', function (done) {
-        chai.request(host)
+        chai.request(SERVER)
             .post(path)
             .set('content-type', 'application/x-www-form-urlencoded')
             .send({ url: 'https://www.google.com' })
